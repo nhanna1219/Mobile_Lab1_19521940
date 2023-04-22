2d629b7814bb3dcd060ce978e26c084324fcd176
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-04-22 16:42:54 +0700

    Done SQLite

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,30 +15,33 @@ import com.example.myapplication.Utils.Models.User;
 import com.example.myapplication.ViewModels.UserVM;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    List<User> users;
     UserVM userVM;
+    DatabaseHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        TextInputLayout name_TI = findViewById(R.id.nameWrapper);
+        TextInputLayout phone_TI = findViewById(R.id.phoneWrapper);
+
         userVM = new ViewModelProvider(this).get(UserVM.class);
 
         Button addLocal_bt = (Button) findViewById(R.id.addLButton);
-        Button addFireStore_bt = (Button) findViewById(R.id.addFSButton);
         Button queryLocal_bt = (Button) findViewById(R.id.queryLButton);
-        Button queryFireStore_bt = (Button) findViewById(R.id.queryFSButton);
 
+        Button addSQLite_bt = (Button) findViewById(R.id.addSQLiteButton);
+        Button querySQLite_bt = (Button) findViewById(R.id.querySQLiteButton);
         addLocal_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextInputLayout name_TI = findViewById(R.id.nameWrapper);
                 String name = Objects.requireNonNull(name_TI.getEditText()).getText().toString();
-                TextInputLayout phone_TI = findViewById(R.id.phoneWrapper);
                 String phone = Objects.requireNonNull(phone_TI.getEditText()).getText().toString();
                 User user = new User(name, phone);
                 userVM.insertUser(user);
@@ -47,16 +51,30 @@ public class MainActivity extends AppCompatActivity {
         queryLocal_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Result.class));
+                Intent i = new Intent(MainActivity.this, Result.class);
+                i.putExtra("queryLocalButton", true);
+                i.putExtra("querySQLiteButton", false);
+                startActivity(i);
             }
         });
 
-        queryFireStore_bt.setOnClickListener(new View.OnClickListener() {
+        addSQLite_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Objects.requireNonNull(name_TI.getEditText()).getText().toString();
+                String phone = Objects.requireNonNull(phone_TI.getEditText()).getText().toString();
+                db = new DatabaseHandler(MainActivity.this);
+                db.insertUser(name, phone);
+            }
+        });
+        querySQLite_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Result.class));
+                Intent i = new Intent(MainActivity.this, Result.class);
+                i.putExtra("queryLocalButton", false);
+                i.putExtra("querySQLiteButton", true);
+                startActivity(i);
             }
         });
     }
-
 }
