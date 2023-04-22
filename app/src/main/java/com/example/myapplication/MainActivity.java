cea316e1909b07c293e4e95a04f8c68184ec77d9
Nhân Nguyễn Trọng <19521940@ms.uit.edu.vn>
2023-04-22 13:39:37 +0700

    Done Firestore (also implement emulators of Firebase)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,22 +16,45 @@ import android.widget.Toast;
 
 import com.example.myapplication.Utils.Models.User;
 import com.example.myapplication.ViewModels.UserVM;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     List<User> users;
     UserVM userVM;
+    private static final String TAG = "MainActivity";
+
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE = "phone";
+
+    private TextInputLayout name_TI;
+    private TextInputLayout phone_TI;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(false)
+            .build();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        db.useEmulator("10.0.2.2", 8080);
+        db.setFirestoreSettings(settings);
+
         userVM = new ViewModelProvider(this).get(UserVM.class);
-        TextInputLayout name_TI = findViewById(R.id.nameWrapper);
-        TextInputLayout phone_TI = findViewById(R.id.phoneWrapper);
+        name_TI = findViewById(R.id.nameWrapper);
+        phone_TI = findViewById(R.id.phoneWrapper);
 
         Button addLocal_bt = (Button) findViewById(R.id.addLButton);
         Button addFireStore_bt = (Button) findViewById(R.id.addFSButton);
@@ -47,16 +74,51 @@ public class MainActivity extends AppCompatActivity {
         queryLocal_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Result.class));
+                Intent i = new Intent(MainActivity.this, Result.class);
+                i.putExtra("localButtonState", true);
+                i.putExtra("queryButtonState", false);
+                startActivity(i);
             }
         });
 
-        queryFireStore_bt.setOnClickListener(new View.OnClickListener() {
+        addFireStore_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Result.class));
+                insertUser();
             }
         });
+        queryFireStore_bt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Result.class);
+                i.putExtra("queryButtonState", true);
+                i.putExtra("localButtonState", false);
+                startActivity(i);
+            }
+        });
+    }
+    protected void insertUser(){
+        String name = Objects.requireNonNull(name_TI.getEditText()).getText().toString();
+        String phone = Objects.requireNonNull(phone_TI.getEditText()).getText().toString();
+        Map<String, Object> user = new HashMap<>();
+        user.put(KEY_NAME, name);
+        user.put(KEY_PHONE, phone);
+
+        db.collection("User").add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this, "Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/example/myapplication/Result.java
+++ app/src/main/java/com/example/myapplication/Result.java
@@ -1,41 +1,96 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.myapplication.Utils.Adapter.CustomAdapter;
 import com.example.myapplication.Utils.Models.User;
 import com.example.myapplication.ViewModels.UserVM;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.core.View;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result extends AppCompatActivity {
     ListView listView;
     CustomAdapter adapter;
     private UserVM userVM;
+    private List<User> users;
+    private FirebaseFirestore db;
+    private CollectionReference userRef;
+    private static final String TAG = "Result";
+
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE = "phone";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        users = new ArrayList<>();
+
         adapter = new CustomAdapter(getApplicationContext());
         listView = findViewById(R.id.customListView);
         listView.setAdapter(adapter);
 
-        userVM = new ViewModelProvider(this).get(UserVM.class);
-        userVM.getAllUsers().observe(this, new Observer<List<User>>() {
-            @Override
-            public void onChanged(List<User> users) {
+        boolean queryButtonState = getIntent().getBooleanExtra("queryButtonState", false);
+        boolean localButtonState = getIntent().getBooleanExtra("localButtonState", false);
+
+        if (queryButtonState) {
+            db = FirebaseFirestore.getInstance();
+            userRef = db.collection("User");
+            loadUser();
+        } else if (localButtonState) {
+            userVM = new ViewModelProvider(this).get(UserVM.class);
+            userVM.getAllUsers().observe(this, new Observer<List<User>>() {
+                @Override
+                public void onChanged(List<User> users) {
 //                Toast.makeText(Result.this, "on changed",Toast.LENGTH_SHORT).show();
-                if(!users.isEmpty()) {
-                    adapter.setUsers(users);
+                    if (!users.isEmpty()) {
+                        adapter.setUsers(users);
+                    }
                 }
-            }
-        });
+            });
+        }
+        ;
+    }
+
+    public void loadUser() {
+        userRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                String name = doc.getString(KEY_NAME);
+                                String phone = doc.getString(KEY_PHONE);
+                                users.add(new User(name,phone));
+                            }
+                            adapter.setUsers(users);
+                        }else {
+                            Toast.makeText(Result.this, "Error!", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG,"Error: ", task.getException());
+                        }
+                    }
+                });
+
     }
 }
